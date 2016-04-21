81bff5ba2c2196f39bc18be0716e7624aae450f5
Franziska Wittleder <franziska.wittleder@gmail.com>
2016-04-21 09:06:07 -0400

    changed api call

--- Fitness.js
+++ Fitness.js
@@ -74,7 +74,7 @@ class Fitness extends Component {
 
     _fetchGymData() {
         /* Fetches the data object from the REST Endpoint and sets it to this.state.dataSource */
-        var endpoint = 'http://cornellpulse.com:8080/api';
+        var endpoint = 'www.cornellpulse.com:3000/api';
 
         fetch(endpoint)
             .then((response) => response.json())
