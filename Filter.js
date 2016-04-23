7be58525d9fdac8a5f2e28a77d216e1e23d0e0f8
Matthew <mcres@dhcp-hol-6167.redrover.cornell.edu>
2016-04-23 12:05:12 -0400

    Semi Fixed Filter (with new names). Only issue left is with Carol's Cafe. Problem likely in rest.api on server.

--- Filter.js
+++ Filter.js
@@ -11,9 +11,9 @@ var {
     } = React;
 
 var cardinalLocations = {
-  'North': ['Bear Necessities', 'Carols Cafe', 'North Star Marketplace', 'Risley', 'RPME', 'Sweet Sensations'],
-  'West': ['Alice Cook House', 'Carl Becker House', 'Jansen\'s Market', 'Jansens at Bethe House', 'Keeton House', 'Kosher', 'Rose House'],
-  'Central': ['Big Red Barn', 'Bus Stop Bagels', 'Cafe Jennie', 'Duffield', 'Goldie\'s Cafe', 'Green Dragon', 'Ivy Room', 'Marthas', 'Okenshields', 'Olin Libe Cafe', 'Rustys', 'Sage', 'Synapsis Cafe', 'Trillium']}
+  'North': ["Bear Necessities Grill & C-Store", "Carol's Cafe", "North Star Dining Room", "Risley Dining Room", "Robert Purcell Marketplace Eatery", "Sweet Sensations"],
+  'West': ["Cook House Dining Room", "Becker House Dining Room", "Jansen's Market", "Jansen's Dining Room at Bethe House", "Keeton House Dining Room", "104West!", "Rose House Dining Room"],
+  'Central': ["Big Red Barn", "Bus Stop Bagels", "Café Jennie", "Mattin's Café", "Goldie's Café", "Green Dragon", "Ivy Room", "Martha's Café", "Okenshields", "Amit Bhatia Libe Café", "Rusty's", "Atrium Café", "Synapsis Café", "Trillium"]}
 
 var Filter = React.createClass({
   getInitialState() {
@@ -37,6 +37,8 @@ var Filter = React.createClass({
   filter(delimiter) {
     if (delimiter != 'All') {
       var cardinal = cardinalLocations[delimiter];
+
+      console.log(this.props.allData);
       
       var filteredList = this.props.allData.filter((el) => 
                                 cardinal.indexOf(el.location) != -1); 
